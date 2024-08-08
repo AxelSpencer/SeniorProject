0b21a8d25ffdbf4de454b16ff561741dfbc73eaa
Sebastian Oquendo <soquendo@email.neit.edu>
2024-08-08 19:37:47 -0400

    barcode tsx

---
+++ HomeTabs/BarcodeScanner.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+const BarcodeScanner: React.FC = () => {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [scanned, setScanned] = useState(false);
+  const [scannedData, setScannedData] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }: { type: string, data: string }) => {
+    setScanned(true);
+    setScannedData(data);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+      )}
+      {scannedData && (
+        <Text style={styles.scannedText}>Scanned Data: {scannedData}</Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  scannedText: {
+    fontSize: 18,
+    marginTop: 10,
+    color: 'white',
+    backgroundColor: 'black',
+    padding: 10,
+  },
+});
+
+export default BarcodeScanner;
