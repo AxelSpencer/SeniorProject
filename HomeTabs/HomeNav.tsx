a2dbac7f842f5118e0be906e1278b5cc384cb286
Sebastian Oquendo <soquendo@email.neit.edu>
2024-08-29 16:44:39 -0400

    barcode scanner working

--- HomeTabs/HomeNav.tsx
+++ HomeTabs/HomeNav.tsx
@@ -4,7 +4,7 @@ import LandingPage from "./LandingPage";
 import SearchPage from "./SearchPage";
 import ResultsPage from "./ResultsPage";
 import BookModal from "./BookModal";
-import ScannerScreen from "./ScannerScreen";
+import ScannerScreen from "./BarcodeScanner";
 
 const BlankPage = () => null;
 
@@ -14,7 +14,7 @@ export type StackParamList = {
   ResultsPage: { query: string };
   BookModal: { book: any };
   ScannerModal: undefined;
-  ScannerScreen: undefined;
+  BarcodeScanner: undefined;
 };
 
 const Stack = createStackNavigator<StackParamList>();
@@ -26,7 +26,7 @@ const HomeNav: React.FC = () => {
       <Stack.Screen name="SearchPage" component={SearchPage} options={{ headerShown: false }} />
       <Stack.Screen name="ResultsPage" component={ResultsPage} options={{ headerShown: false }} />
       <Stack.Screen name="BookModal" component={BookModal} options={{ presentation: "transparentModal", headerShown: false }} />
-      <Stack.Screen name="ScannerScreen" component={ScannerScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="BarcodeScanner" component={ScannerScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
